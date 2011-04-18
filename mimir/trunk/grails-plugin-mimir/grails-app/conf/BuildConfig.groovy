cbb147e96498b77c0029d4983b00fda16f1b8c2b
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-04-18 15:48:14 +0000

    formatting

--- mimir/trunk/grails-plugin-mimir/grails-app/conf/BuildConfig.groovy
+++ mimir/trunk/grails-plugin-mimir/grails-app/conf/BuildConfig.groovy
@@ -16,7 +16,7 @@
  *  $Id$
  */
 
- grails.project.class.dir = "target/classes"
+grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
