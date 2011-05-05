f84b4ad53a4cd90f71a27f09fe41eb7fb3320f99
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-05-05 10:55:58 +0000

    Added copyright notices to all source files; Re-formatted source files to follow official guidelines (e.g. spaces-for-tabs).

--- mimir/trunk/plugins/sesame/src/gate/mimir/sesame/SesamePluginResource.java
+++ mimir/trunk/plugins/sesame/src/gate/mimir/sesame/SesamePluginResource.java
@@ -1,3 +1,20 @@
+/*
+ * SesamePluginResource.java
+ * 
+ * Copyright (c) 2007-2011, The University of Sheffield.
+ * 
+ * This file is part of GATE Mímir (see http://gate.ac.uk/family/mimir.html),
+ * and is free software, licenced under the GNU Affero General Public License,
+ * Version 3, November 2007 (also included with this distribution as file
+ * LICENCE-AGPL3.html).
+ * 
+ * A commercial licence is also available for organisations whose business
+ * models preclude the adoption of open source and is subject to a licence fee
+ * charged by the University of Sheffield. Please contact the GATE team (see
+ * http://gate.ac.uk/g8/contact) if you require a commercial licence.
+ * 
+ * $Id$
+ */
 package gate.mimir.sesame;
 
 import gate.Resource;
@@ -7,20 +24,17 @@ import gate.creole.metadata.CreoleResource;
 
 /**
  * A simple no-function CREOLE resource only used so that we have some CREOLE
- * metadata. We use that to locate the JAR containing the classes, which we 
- * then use to locate the associated resource files.   
+ * metadata. We use that to locate the JAR containing the classes, which we then
+ * use to locate the associated resource files.
  */
-@CreoleResource(isPrivate=true, comment="A resource type only defined to use " +
-		"its configuration side effects. It cannot be instantiated!")
-public class SesamePluginResource extends AbstractResource{
-	
-	
-	private static final long serialVersionUID = -3138987553373647753L;
+@CreoleResource(isPrivate = true, comment = "A resource type only defined to use "
+        + "its configuration side effects. It cannot be instantiated!")
+public class SesamePluginResource extends AbstractResource {
+  private static final long serialVersionUID = -3138987553373647753L;
 
-	@Override
-	  public Resource init() throws ResourceInstantiationException {
-	    throw new ResourceInstantiationException("This resource has no actual " +
-	    		"function and cannot be instantaited.");
-	  }
-
+  @Override
+  public Resource init() throws ResourceInstantiationException {
+    throw new ResourceInstantiationException("This resource has no actual "
+            + "function and cannot be instantaited.");
+  }
 }
--- mimir/trunk/plugins/sesame/src/gate/mimir/sesame/SesameUtils.java
+++ mimir/trunk/plugins/sesame/src/gate/mimir/sesame/SesameUtils.java
@@ -1,52 +1,68 @@
+/*
+ * SesameUtils.java
+ * 
+ * Copyright (c) 2007-2011, The University of Sheffield.
+ * 
+ * This file is part of GATE Mímir (see http://gate.ac.uk/family/mimir.html),
+ * and is free software, licenced under the GNU Affero General Public License,
+ * Version 3, November 2007 (also included with this distribution as file
+ * LICENCE-AGPL3.html).
+ * 
+ * A commercial licence is also available for organisations whose business
+ * models preclude the adoption of open source and is subject to a licence fee
+ * charged by the University of Sheffield. Please contact the GATE team (see
+ * http://gate.ac.uk/g8/contact) if you require a commercial licence.
+ * 
+ * $Id$
+ */
 package gate.mimir.sesame;
 
-
 public class SesameUtils {
-	/**
-	   * Key used to retrieve the Sesame RepositoryManager source from the 
-	   * {@link IndexConfig#getContext()} context map.
-	   */
-	  public static final String SESAME_RMANAGER_KEY = "repositoryManager";
+  /**
+   * Key used to retrieve the Sesame RepositoryManager source from the
+   * {@link IndexConfig#getContext()} context map.
+   */
+  public static final String SESAME_RMANAGER_KEY = "repositoryManager";
 
-	  /**
-	   * Key used to retrieve an Integer value, representing the number of
-	   * Repository connections.
-	   */
-	  public static final String SESAME_CONNECTION_COUNT_KEY = "sesameRepositoryConnectionCount";
-	  
-	  /**
-	   * The name of the index subdirectory storing Sesame repository data.
-	   */
-	  public static final String SESAME_INDEX_DIRNAME = "sesame";
-	  
-	  /**
-	   * The name of the config file for the Sesame repository
-	   */
-	  public static final String SESAME_CONFIG_FILENAME = ".ttl";
-	  
-	  /**
-	   * The key to retrieve the name(id) of the Sesame repository
-	   */
-	  public static final String SESAME_REPOSITORY_NAME_KEY = "sesameRepository";
-	  
-	  /**
-	   * The namespace used for Mimir-specific URIs.
-	   */
-	  public static final String MIMIR_NAMESPACE = "urn:mimir:";
+  /**
+   * Key used to retrieve an Integer value, representing the number of
+   * Repository connections.
+   */
+  public static final String SESAME_CONNECTION_COUNT_KEY =
+          "sesameRepositoryConnectionCount";
 
-	  /**
-	   * Predicate string.
-	   */
-	  public static final String HAS_FEATURES = "hasFeatures";
+  /**
+   * The name of the index subdirectory storing Sesame repository data.
+   */
+  public static final String SESAME_INDEX_DIRNAME = "sesame";
 
-	  /**
-	   * Predicate string.
-	   */
-	  public static final String HAS_MENTION = "hasMention";
+  /**
+   * The name of the config file for the Sesame repository
+   */
+  public static final String SESAME_CONFIG_FILENAME = ".ttl";
 
-	  /**
-	   * Predicate string.
-	   */
-	  public static final String HAS_LENGTH = "hasLength";
-	
+  /**
+   * The key to retrieve the name(id) of the Sesame repository
+   */
+  public static final String SESAME_REPOSITORY_NAME_KEY = "sesameRepository";
+
+  /**
+   * The namespace used for Mimir-specific URIs.
+   */
+  public static final String MIMIR_NAMESPACE = "urn:mimir:";
+
+  /**
+   * Predicate string.
+   */
+  public static final String HAS_FEATURES = "hasFeatures";
+
+  /**
+   * Predicate string.
+   */
+  public static final String HAS_MENTION = "hasMention";
+
+  /**
+   * Predicate string.
+   */
+  public static final String HAS_LENGTH = "hasLength";
 }
