1986a0cfb75ef255973050d50b87099744069dfd
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-08-17 12:56:32 +0000

    Rolling back the NPE guard as it was already independently implemented (a few lines below).

--- mimir-core/src/gate/mimir/util/DocumentFeaturesMetadataHelper.java
+++ mimir-core/src/gate/mimir/util/DocumentFeaturesMetadataHelper.java
@@ -1,22 +1,21 @@
 /*
- * DocumentFeaturesMetadataHelper.java
- * 
- * Copyright (c) 2007-2011, The University of Sheffield.
- * 
- * This file is part of GATE Mímir (see http://gate.ac.uk/family/mimir.html),
- * and is free software, licenced under the GNU Affero General Public License,
- * Version 3, November 2007 (also included with this distribution as file
- * LICENCE-AGPL3.html).
- * 
- * A commercial licence is also available for organisations whose business
- * models preclude the adoption of open source and is subject to a licence fee
- * charged by the University of Sheffield. Please contact the GATE team (see
- * http://gate.ac.uk/g8/contact) if you require a commercial licence.
- * 
- * Valentin Tablan, 6 Oct 2009
- * 
- * $Id: DocumentFeaturesMetadataHelper.java 14241 2011-08-12 15:59:27Z
- * ian_roberts $
+ *  DocumentFeaturesMetadataHelper.java
+ *
+ *  Copyright (c) 2007-2011, The University of Sheffield.
+ *
+ *  This file is part of GATE Mímir (see http://gate.ac.uk/family/mimir.html), 
+ *  and is free software, licenced under the GNU Affero General Public License,
+ *  Version 3, November 2007 (also included with this distribution as file 
+ *  LICENCE-AGPL3.html).
+ *
+ *  A commercial licence is also available for organisations whose business
+ *  models preclude the adoption of open source and is subject to a licence
+ *  fee charged by the University of Sheffield. Please contact the GATE team
+ *  (see http://gate.ac.uk/g8/contact) if you require a commercial licence.
+ *
+ *  Valentin Tablan, 6 Oct 2009
+ *
+ *  $Id$
  */
 package gate.mimir.util;
 
@@ -31,47 +30,47 @@ import gate.mimir.index.mg4j.GATEDocument;
 import gate.mimir.index.mg4j.zipcollection.DocumentData;
 import gate.mimir.search.QueryEngine;
 
+
 /**
- * A simple {@link DocumentMetadataHelper} that copies the values of some GATE
- * document features as metadata fields in the index. Note that the values of
+ * A simple {@link DocumentMetadataHelper} that copies the values of some GATE 
+ * document features as metadata fields in the index. Note that the values of 
  * the specified features must be {@link Serializable}; values that are not will
- * not be saved in the index. The values thus saved can be retrieved at search
- * time by calling {@link QueryEngine#getDocumentMetadataField(int, String)}.
+ * not be saved in the index.
+ * 
+ * The values thus saved can be retrieved at search time by calling 
+ * {@link QueryEngine#getDocumentMetadataField(int, String)}.
  */
 public class DocumentFeaturesMetadataHelper implements DocumentMetadataHelper {
+  
   /**
-   * A map storing the correspondence between the GATE document feature name and
-   * the metadata field name in the Mimir index.
+   * A map storing the correspondence between the  GATE document feature name 
+   * and the metadata field name in the Mimir index. 
    */
   protected Map<String, String> featureNameToFieldName;
-
-  private static Logger logger = Logger
-    .getLogger(DocumentFeaturesMetadataHelper.class);
-
+  
+  private static Logger logger = Logger.getLogger(
+          DocumentFeaturesMetadataHelper.class);
+  
   /**
    * Creates a new DocumentFeaturesMetadataHelper.
-   * 
-   * @param featureNameToFieldName
-   *          a map storing the correspondence between the GATE document feature
-   *          name and the metadata field name; keys are names of document
-   *          features; values are names of metadata fields.
+   * @param featureNameToFieldName a map storing the correspondence between the 
+   * GATE document feature name and the metadata field name; keys are names of
+   * document features; values are names of metadata fields. 
    */
-  public DocumentFeaturesMetadataHelper(Map<String, String> featureNameToFieldName) {
+  public DocumentFeaturesMetadataHelper(
+          Map<String, String> featureNameToFieldName) {
     this.featureNameToFieldName = featureNameToFieldName;
   }
-
+  
   /**
    * Creates a new DocumentFeaturesMetadataHelper.
-   * 
-   * @param featureNames
-   *          an array of feature names. For each indexed document, the values
-   *          for the features specified here are obtained and stored in the
-   *          index, as document metadata fields with the same names as the GATE
-   *          document features. If you need the names of the Mimir document
-   *          metadata fields to be different from the GATE document features,
-   *          then you should use the
-   *          {@link #DocumentFeaturesMetadataHelper(Map)} variant.
-   */
+   * @param featureNames an array of feature names. For each indexed document,
+   * the values for the features specified here are obtained and stored in the
+   *  index, as document metadata fields with the same names as the GATE 
+   *  document features. If you need the names of the Mimir document metadata
+   *  fields to be different from the GATE document features, then you should 
+   *  use the {@link #DocumentFeaturesMetadataHelper(Map)} variant. 
+   */  
   public DocumentFeaturesMetadataHelper(String... featureNames) {
     this.featureNameToFieldName = new HashMap<String, String>();
     for(String f : featureNames) {
@@ -88,18 +87,15 @@ public class DocumentFeaturesMetadataHelper implements DocumentMetadataHelper {
   public void documentEnd(GATEDocument document, DocumentData documentData) {
     for(Map.Entry<String, String> mapping : featureNameToFieldName.entrySet()) {
       Object value = document.getDocument().getFeatures().get(mapping.getKey());
-      if(value != null) {
-        if(value instanceof Serializable) {
-          documentData.putMetadataField(mapping.getValue(), 
-            (Serializable)value);
-        } else if(value != null) { // null is not an instanceof anything
-          logger.warn("Value for document feature \"" + mapping.getKey() +
-            "\" on document with title \"" +
-            (document.title() == null ? "<null>" : document.title()) +
-            "\", and URI: \"" +
-            (document.uri() == null ? "<null>" : document.uri()) +
-            "\" is not serializable. Document metadata field NOT saved.");
-        }
+      if(value instanceof Serializable) {
+        documentData.putMetadataField(mapping.getValue(), (Serializable)value);
+      } else if(value != null) { // null is not an instanceof anything
+        logger.warn("Value for document feature \"" + mapping.getKey() + 
+                "\" on document with title \"" + 
+                (document.title() == null ? "<null>" : document.title()) +
+                "\", and URI: \"" +
+                (document.uri() == null ? "<null>" : document.uri()) +
+                "\" is not serializable. Document metadata field NOT saved.");
       }
     }
   }
