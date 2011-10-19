9dc2396309948825143327cf0ec896bc5967c1d2
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2011-10-19 13:41:58 +0000

    New 3.3.0 version.

--- mimir/trunk/grails-plugin-mimir/MimirWebGrailsPlugin.groovy
+++ mimir/trunk/grails-plugin-mimir/MimirWebGrailsPlugin.groovy
@@ -31,7 +31,7 @@ class MimirWebGrailsPlugin {
     static Logger log = Logger.getLogger('gate.mimir.mimir-web')
   
     // the plugin version
-    def version = "3.2.1-snapshot"
+    def version = "3.3.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.5 > *"
     // the other plugins this plugin depends on
