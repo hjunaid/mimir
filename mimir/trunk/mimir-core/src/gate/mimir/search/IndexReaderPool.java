8114639883fcf430a606f3b8e7df3a34605fefd5
valyt <valyt@aa4978b9-1eff-4426-90b4-889a60d71948>
2013-02-28 11:16:47 +0000

    New terms query type: TermTypeTermsQuery which enumerates all term of a given type found in the whole index. A term type is either a token feature name or an annotation type.
    IndexReaderPool: added support for enumerating all the terms in the dictionary of an index, and for getting their global counts. This is used by the new TermTypeTermsQuery class.

--- mimir/trunk/mimir-core/src/gate/mimir/search/IndexReaderPool.java
+++ mimir/trunk/mimir-core/src/gate/mimir/search/IndexReaderPool.java
@@ -19,9 +19,15 @@ import it.unimi.dsi.big.mg4j.index.DiskBasedIndex;
 import it.unimi.dsi.big.mg4j.index.Index;
 import it.unimi.dsi.big.mg4j.index.IndexReader;
 import it.unimi.dsi.big.util.ImmutableExternalPrefixMap;
+import it.unimi.dsi.big.util.SemiExternalGammaBigList;
+import it.unimi.dsi.fastutil.longs.LongBigList;
+import it.unimi.dsi.fastutil.objects.ObjectBigList;
+import it.unimi.dsi.fastutil.objects.ObjectIterator;
+import it.unimi.dsi.io.InputBitStream;
 import it.unimi.dsi.lang.MutableString;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -32,32 +38,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class IndexReaderPool {
   
-  private class IndexDictionary extends ImmutableExternalPrefixMap {
-
-    private static final long serialVersionUID = 3350171413268036811L;
-
-    public IndexDictionary(Iterable<? extends CharSequence> terms)
-      throws IOException {
-      super(terms);
-    }
-
-
-    /**
-     * Gets the term string for a given term ID. This method is synchronised to
-     * protect the shared mutable string instance.
-     * @param termId the ID for the term being sought.
-     * @return the string for the given term.
-     */
-    public String getTerm(long termId) {
-      return super.getTerm(termId, new MutableString()).toString();
-    }
-    
-  }
-  
   private static final int DEFAULT_CAPACITY = 100000;
   
   /**
-   * How many readers are norally kept. The actual size can be much larger than
+   * How many readers are normally kept. The actual size can be much larger than
    * this, as this pool will never refuse to create new readers. However, upon 
    * being returned, all excess readers get destroyed instead of being 
    * re-queued.
@@ -85,7 +69,13 @@ public class IndexReaderPool {
    * An external map that holds the index terms. Allows the retrieval of a 
    * term's text given a term ID.
    */
-  private IndexDictionary dictionary;
+  private transient ImmutableExternalPrefixMap dictionary;
+  
+  /**
+   * A semi-external list holding the global counts for the terms in this 
+   * index's dictionary.
+   */
+  private transient LongBigList termOccurrenceCounts;
   
   /**
    * The actual pool.
@@ -141,6 +131,22 @@ public class IndexReaderPool {
     return index;
   }
   
+  
+  /**
+   * Gets the terms dictionary for this index.
+   * @throws IOException
+   * @return the index dictionary 
+   */
+  public ImmutableExternalPrefixMap getDictionary() throws IOException {
+    if(dictionary == null) {
+      FileLinesCollection dictionaryLines = new FileLinesCollection(
+              new File(URI.create(indexURI.toString() + 
+                DiskBasedIndex.TERMS_EXTENSION)).getAbsolutePath(), "UTF-8");
+            dictionary = new ImmutableExternalPrefixMap(dictionaryLines);
+    }
+    return dictionary;
+  }
+  
   /**
    * Gets the string term for a given term ID.
    * @param termId
@@ -148,14 +154,39 @@ public class IndexReaderPool {
    * @throws IOException
    */
   public String getTerm(long termId) throws IOException {
-    if(dictionary == null) {
-      FileLinesCollection dictionaryLines = new FileLinesCollection(
-        new File(URI.create(indexURI.toString() + 
-          DiskBasedIndex.TERMS_EXTENSION)).getAbsolutePath(), "UTF-8");
-      dictionary = new IndexDictionary(dictionaryLines);
-    }
-    return dictionary.getTerm(termId);
+    return getDictionary().list().get(termId).toString();
   }
+  
+  /**
+   * Gets the list of terms in this index. 
+   * @return a list of terms represented as {@link MutableString} values.
+   * @throws IOException
+   */
+  public ObjectBigList<MutableString> getTermsList() throws IOException {
+    return getDictionary().list();
+  }
+  
+  /**
+   * Gets the list of term occurrence counts (global counts for each term in the
+   * whole index). 
+   * @return a (big, i.e. 64-bit indexed) list of long values.
+   * @throws IOException
+   */
+  public LongBigList getTermOccurenceCounts() throws IOException {
+    if(termOccurrenceCounts == null) {
+      File countsFile = new File(URI.create(indexURI.toString() + 
+              DiskBasedIndex.GLOBCOUNTS_EXTENSION));
+      if(!countsFile.exists()) {
+        // when we change to MG4J 5+ indexes, this will have to change to use
+        // the new .occurrences extension
+        throw new IOException("Could not find occurrences file at " + 
+            countsFile.getAbsolutePath() + "!");
+      }
+      termOccurrenceCounts = new SemiExternalGammaBigList(
+          new InputBitStream(countsFile), -1, getDictionary().size64());
+    }
+    return termOccurrenceCounts;
+  }  
   
   public void close() throws IOException {
     IndexReader aReader = pool.poll();
